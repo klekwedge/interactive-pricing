849f6a0b66b94d00b758ec67861691099e3e1ff0
Andrey <antonov.andrey.10101@gmail.com>
2023-09-16 14:42:12 +0300

    feat: add BillingInfo

---
+++ src/components/BillingInfo.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="billing-info">
+    <p class="billing-info--monthly">Monthly Billing</p>
+    <Toggle />
+    <p class="billing-info--yearly">Yearly Billing</p>
+    <p class="billing-info--discount">25% discount</p>
+    <p class="billing-info--discount-mobile">25%</p>
+  </div>
+</template>
+
+<script>
+import Toggle from "./Toggle.vue";
+
+export default {
+  components: {
+    Toggle,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.billing-info {
+  display: flex;
+  align-items: center;
+  font-size: 0.8rem;
+
+  .billing-info--monthly,
+  .billing-info--yearly {
+    color: $neutral-grayish-blue;
+  }
+
+  .billing-info--monthly {
+    margin-right: 15px;
+  }
+
+  .billing-info--yearly {
+    margin-left: 15px;
+  }
+
+  .billing-info--discount {
+    background: $primary-light-grayish-red;
+    color: $primary-light-red;
+    padding: 3px 7px;
+    border-radius: 50px;
+    font-size: 0.6rem;
+    font-weight: $extra-bold;
+    margin-left: 10px;
+  }
+}
+
+.billing-info--discount-mobile {
+  display: none;
+}
+
+@media only screen and (max-width: $mobile) {
+  .billing-info {
+    margin-left: 35px;
+
+    .billing-info--discount {
+      display: none;
+    }
+
+    .billing-info--discount-mobile {
+      display: block;
+      background: $primary-light-grayish-red;
+      color: $primary-light-red;
+      padding: 3px 7px;
+      border-radius: 50px;
+      font-size: 0.6rem;
+      font-weight: $extra-bold;
+      margin-left: 10px;
+    }
+
+    .billing-info--monthly,
+    .billing-info--yearly {
+      font-size: 0.65rem;
+    }
+
+    .billing-info--monthly {
+      margin-right: 10px;
+    }
+
+    .billing-info--yearly {
+      margin-left: 10px;
+    }
+  }
+}
+</style>
