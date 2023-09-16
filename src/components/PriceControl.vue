462bb095d12a5a235830107f855247f48bffef7f
Andrey <antonov.andrey.10101@gmail.com>
2023-09-16 14:43:38 +0300

    feat: add PriceControl

---
+++ src/components/PriceControl.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="price-control">
+        <div class="price-control__values">
+            <PageViews />
+            <Price />
+        </div>
+        <div class="price-control__controls">
+            <RangeSlider class="price-control__controls--slider" />
+            <BillingInfo class="price-control__controls--billing-info" />
+        </div>
+    </div>
+    <div class="price-control--mobile">
+        <PageViews class="price-control--mobile__page-views" />
+        <RangeSlider class="price-control--mobile__slider" />
+        <Price class="price-controle--mobile__price" />
+        <BillingInfo class="price-control--mobile__billing-info" />
+    </div>
+</template>
+
+<script>
+import PageViews from './PageViews.vue'
+import Price from './Price.vue'
+import RangeSlider from './RangeSlider.vue'
+import BillingInfo from './BillingInfo.vue'
+
+export default {
+    components: {
+        PageViews,
+        Price,
+        RangeSlider,
+        BillingInfo,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.price-control {
+    padding: 50px;
+    border-bottom: 1px solid $neutral-light-grayish-blue;
+    display: block;
+
+    .price-control__values {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 30px;
+    }
+
+    .price-control__controls {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+
+        .price-control__controls--slider {
+            margin-bottom: 50px;
+        }
+
+        .price-control__controls--billing-info {
+            margin: 0 40px 0 0;
+        }
+    }
+}
+
+.price-control--mobile {
+    display: none;
+}
+
+@media only screen and (max-width: $mobile) {
+    .price-control {
+        display: none;
+    }
+    .price-control--mobile {
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .price-control--mobile__page-views,
+        .price-control--mobile__slider,
+        .price-controle--mobile__price {
+            margin-bottom: 30px;
+        }
+    }
+}
+</style>
